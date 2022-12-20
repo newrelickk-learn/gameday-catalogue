788e4b93e8d7bc07304eaf620a6d6b9158335318
Takuya Azuma <tazuma@newrelic.com>
2022-12-20 23:32:20 +0900

    error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+    rescue_from Exception, with: :handle_exception
+    def handle_exception(_e)
+      raise _e.message.gsub(/"/, '')
+    end
 end
