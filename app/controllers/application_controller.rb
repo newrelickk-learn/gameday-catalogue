5fbd15772a3a7e104f0375364ad73f6124c9bdac
Takuya Azuma <tazuma@newrelic.com>
2022-12-21 09:30:59 +0900

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     rescue_from Exception, with: :handle_exception
     def handle_exception(_e)
-      raise _e.message.gsub(/"/, '')
+      raise Exception, _e.message.gsub(/"/, ''), _e.backtrace
     end
 end
