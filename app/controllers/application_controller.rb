27a89ca0ae71765e7cfd28ec8b9c4239e370693b
Takuya Azuma <tazuma@newrelic.com>
2023-11-17 15:39:28 +0900

    fix deployment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
+    before_action :tracking
     rescue_from Exception, with: :handle_exception
     def handle_exception(_e)
       raise Exception, _e.message.gsub(/"/, ''), _e.backtrace
+    end
+
+    def tracking
+        @userParam = params[:user]
+        ::NewRelic::Agent.add_custom_attributes({ user: params[:user] })
     end
 end
