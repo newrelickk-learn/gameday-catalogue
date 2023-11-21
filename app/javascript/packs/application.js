96b2c326a84712763c2ac199d99fe48eed60692d
Takuya Azuma <tazuma@newrelic.com>
2023-11-21 09:40:50 +0900

    サービスリリース

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,13 +28,12 @@ function bindFunctions() {
 }
 async function makeSomeRequest () {
     const user = location.search.split(/(\?|&)/).find(p=>p.startsWith('user')) ?? ''
-    await fetch(`/catalogue/size?tags=blue,magic&${user}`)
-    for(let i= 0; i < 2; i++) {
+    try {
+        await fetch(`/catalogue/size?tags[]=blue&tags[]=magic&${user}`)
+    } catch {}
+    try {
         await fetch(`/catalogue?tags[]=blue&tags[]=magic&${user}`)
-    }
-    for(let i= 0; i < 5; i++) {
-        await fetch(`/catalogue?tags=blue,magic&${user}`)
-    }
+    } catch {}
 }
 
 function method1() {
