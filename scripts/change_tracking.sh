b7f610181a85cdf3055764571cfabd63b117d260
Takuya Azuma <tazuma@newrelic.com>
2023-11-22 14:41:55 +0900

    N+1問題の修正

--- scripts/change_tracking.sh
+++ scripts/change_tracking.sh
@@ -4,7 +4,7 @@ for i in `seq 1 100`; do
   echo "try #$i";
   RES=$(curl -X POST https://api.newrelic.com/graphql -H 'Content-Type: application/json' -H 'API-Key: '${NEW_RELIC_USER_KEY} --data @scripts/running1stPod.query)
   echo $RES
-  if [ -z `echo $RES | grep count:"1"` ]; then
+  if [ -z `echo $RES | grep "count":1` ]; then
     echo "No pod running yet";
   else
     echo "Some Pod Running";
