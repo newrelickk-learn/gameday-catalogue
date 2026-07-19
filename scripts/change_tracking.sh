eef3800d32bf7bb171c3f20884186d3f08a75fdf
Azumaxoid <azumax.android@gmail.com>
2026-07-19 21:39:03 +0900

    change pod running check

--- scripts/change_tracking.sh
+++ scripts/change_tracking.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 NEW_RELIC_USER_KEY=${1:-KEY}
+NAMESPACE=catalogue
+DEPLOYMENT_LABEL=catalogue-web
+IMAGE=$(grep -m1 'image:' deployment.yaml | sed 's/.*image: *//')
+
 for i in `seq 1 100`; do
   echo "try #$i";
-  RES=$(curl -X POST https://api.newrelic.com/graphql -H 'Content-Type: application/json' -H 'API-Key: '${NEW_RELIC_USER_KEY} --data @scripts/running1stPod.query)
-  echo $RES
-  if [ -z `echo $RES | grep \"count\":1` ]; then
-    echo "No pod running yet";
-  else
+  RUNNING_COUNT=$(kubectl get pods -n ${NAMESPACE} -l name=${DEPLOYMENT_LABEL} --field-selector=status.phase=Running -o json \
+    | jq -r --arg img "${IMAGE}" '[.items[] | select(.spec.containers[].image == $img)] | length')
+  echo "Running pods with image ${IMAGE}: ${RUNNING_COUNT}"
+  if [ "${RUNNING_COUNT}" -ge 1 ]; then
     echo "Some Pod Running";
     break;
+  else
+    echo "No pod running yet";
   fi;
   sleep 30;
 done
